2715ddf48a3a4ba0d40fada337cae1c7feef0add
Adam Wolfman <adamwolfman@Adams-MacBook-Pro.local>
2019-04-11 17:36:56 -0600

    homework completed

---
+++ models/marsMissions.js
@@ -0,0 +1,39 @@
+const marsMissions = [
+    {
+      name: "Curiosity",
+      launchDate: "26 Nov 2011",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://mars.nasa.gov/imgs/2016/06/mars-curiosity-rover-okoruso-drill-hole-self-portrait-PIA20602-PIA20603-fi.gif"
+    },
+    {
+      name: "Opportunity",
+      launchDate: "8 Jul 2003",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://media.istockphoto.com/photos/yellow-opportunity-ahead-road-sign-with-sky-picture-id471741807?k=6&m=471741807&s=612x612&w=0&h=E42-mwOPvSBN0mFoWr4xG0ZqdJ3PbouvAVSTHB2wRLg="
+    },
+    {
+      name: "Spirit",
+      launchDate: "10 Jun 2003",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKI4dXTEZTR0EDqQi_2a144ElGlXQoxijrzXDLE4DiMiDZ-AEefQ"
+    },
+    {
+      name: "Sojourner",
+      launchDate: "4 Dec 1996",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sojourner_on_Mars_PIA01122.jpg/260px-Sojourner_on_Mars_PIA01122.jpg"
+    },
+    {
+      name: "Rosetta",
+      launchDate: "2 Mar 2004",
+      operator: "ESA",
+      missionType: "Gravity Assist",
+      img: "https://i.kinja-img.com/gawker-media/image/upload/s--7Wrx8-51--/c_scale,f_auto,fl_progressive,q_80,w_800/g8apqhahsyrks60xswb6.jpg"
+    }
+  ];
+
+  module.exports = marsMissions;
